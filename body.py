d7e4bf10e5e5d76f7b44db4fcb64fd6135c44ab9
smnov <101511388+smnov@users.noreply.github.com>
2022-04-26 13:26:15 +0800

    classes player and enemy

--- body.py
+++ body.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame, random, os
 
 pygame.init()
 pygame.font.init() 
@@ -8,18 +8,44 @@ pygame.display.set_caption('Priest Beast')
 clock = pygame.time.Clock()
 
 BG = pygame.transform.scale2x(pygame.image.load('art/background.png')).convert_alpha()
-# Player
-Player_img1 = pygame.transform.scale2x(pygame.image.load('art/Player.png')).convert_alpha()
-Player_rect = Player_img1.get_rect(center = (800, 300))
+
 music = pygame.mixer.Sound('sound/music.mp3')
 
+class Player(pygame.sprite.Sprite):
 
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale2x(pygame.image.load('art/Player.png'))
+        self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-#Enemies
-Enemy_img = pygame.image.load('art/Enemy.png').convert_alpha()
-Enemy_rect = Enemy_img.get_rect(midbottom = (random.randint(-400, -100), 300))
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.rect.x += 4
+        if keys[pygame.K_a]:
+            self.rect.x -= 4
+        if player <= -30:
+            self.rect.x = -30
+        if player >= 810:
+            self.rect.x = 810
 
+class Enemy(pygame.sprite.Sprite):
 
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale2x(pygame.image.load('art/Enemy.png'))
+        self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+    def move(self):
+        self.rect.x += 6
+        if self.rect.x > 950:
+            self.kill
 
 #Loop and exit
 while True:
@@ -27,37 +53,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+   
     # Sounds
     music.play()
     music.set_volume(0.1)
 
-    #Enemy Spawn
-    
-    enemy_list = []
-    SPAWNENEMY = pygame.USEREVENT
-    pygame.time.set_timer(SPAWNENEMY,1000)
-    enemy_list.append(Enemy_rect)
-        
-
-    # Player
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        Player_rect.x += 4
-    if keys[pygame.K_a]:
-        Player_rect.x -= 4
-    if Player_rect.x <= -30:
-        Player_rect.x = -30
-    if Player_rect.x >= 810:
-        Player_rect.x = 810
-
     screen.blit(BG, (0, 0))
-    screen.blit(Player_img1, Player_rect)
-    screen.blit(Enemy_img, Enemy_rect)
-    
 
-  
+    player = Player(800, 220)
+    player_list = pygame.sprite.Group()
+    player_list.add(player)
+    player_list.draw(screen)
+
+    enemy = Enemy(100,200)    # spawn enemy
+    enemy_list = pygame.sprite.Group()   # create enemy group
+    enemy_list.add(enemy)  
+    enemy_list.draw(screen) #refresh enemy
 
     #Update everything
+    enemy_list.update()
+    player_list.update()
     pygame.display.update()
     clock.tick(60)
