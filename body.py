73acb3380eeca3f20e5bfa9c8e7bf6af933ed5af
smnov <101511388+smnov@users.noreply.github.com>
2022-04-26 10:40:35 +0800

    added enemies

--- body.py
+++ body.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, random
 
 pygame.init()
 pygame.font.init() 
@@ -8,9 +8,16 @@ pygame.display.set_caption('Priest Beast')
 clock = pygame.time.Clock()
 
 BG = pygame.transform.scale2x(pygame.image.load('art/background.png')).convert_alpha()
+# Player
 Player_img1 = pygame.transform.scale2x(pygame.image.load('art/Player.png')).convert_alpha()
 Player_rect = Player_img1.get_rect(center = (800, 300))
 music = pygame.mixer.Sound('sound/music.mp3')
+
+
+
+#Enemies
+Enemy_img = pygame.image.load('art/Enemy.png').convert_alpha()
+Enemy_rect = Enemy_img.get_rect(midbottom = (random.randint(-400, -100), 300))
 
 
 
@@ -20,8 +27,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # Sounds
     music.play()
     music.set_volume(0.1)
+
+    #Enemy Spawn
+    
+    enemy_list = []
+    SPAWNENEMY = pygame.USEREVENT
+    pygame.time.set_timer(SPAWNENEMY,1000)
+    enemy_list.append(Enemy_rect)
+        
+
     # Player
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
@@ -35,6 +53,8 @@ while True:
 
     screen.blit(BG, (0, 0))
     screen.blit(Player_img1, Player_rect)
+    screen.blit(Enemy_img, Enemy_rect)
+    
 
   
 
